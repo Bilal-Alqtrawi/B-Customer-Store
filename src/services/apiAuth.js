3e5b5b16d955ed436d686130e0d4e87d6bd78de4
Bilal-Alqtrawi <bbill4552@gmail.com>
2025-10-15 15:46:55 +0300

    chor: update in service of authApi

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -65,7 +65,7 @@ export async function signInWithProvider(provider, remember = true) {
   const { data, error } = await Supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: window.location.origin,
+      redirectTo: "https://b-customer-store.vercel.app/",
     },
   });
 
